fb81bd9f63a5a14ac721b13f08b3f4840cfaebbe
wra <lovemode1993@naver.com>
2020-01-20 13:52:07 +0900

    S02P12A305-4 Setting

---
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/SsafysnsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafysnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafysnsApplication.class, args);
+	}
+
+}
---
+++ Backend/SSAFYSNS/src/test/java/com/ssafysns/SsafysnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafysnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
