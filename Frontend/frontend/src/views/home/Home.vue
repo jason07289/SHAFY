8b9a627e8f2c4e77a24c31bbcc449bb80474f90e
SOOJIN <soojin4964@gmail.com>
2020-01-21 12:15:26 +0900

    S02P12A305 S02P12A305-62 Actions (Type . to access issue actions) Resolves test close issue

--- Frontend/frontend/src/views/home/Home.vue
+++ Frontend/frontend/src/views/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
+    <Navbar></Navbar>
     <h1>홈 화면</h1>
   </div>
 </template>
 
 <script>
+import Navbar from '../../components/Navbar'
 export default {
-
+  components:{
+    Navbar,
+  }
 }
 </script>
 
