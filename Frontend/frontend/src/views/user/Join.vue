7a867879f90ed5302f12d2677e36837b5c29c7d7
milkbean <nim950313@gmail.com>
2020-01-21 17:53:35 +0900

    S02P12A305 S02P12A305-97 select page 텍스트채움

--- Frontend/frontend/src/views/user/Join.vue
+++ Frontend/frontend/src/views/user/Join.vue
@@ -2,8 +2,8 @@
 <template>
 
     <div>
-        <joinType main-title="타이틀입니다" sub-title="서브타이틀 입니다" />
-        <joinType main-title="타이틀2입니다" sub-title="서브타이틀2 입니다" />
+        <joinType main-title="교육생 가입" sub-title="삼성 청년 SW 아카데미 교육생으로 입과한 적이 있는 경우" />
+        <joinType main-title="비 교육생 가입" sub-title="운영진, 취업지원센터, 수업 강사 등<br>혹은 삼성 청년 SW 아카데미에 관심을 가지고 있는 분" />
 
     </div>
     
