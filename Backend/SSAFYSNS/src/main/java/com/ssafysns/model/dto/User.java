6306df9a4d8cc032f681e89d2f8662c072e0a55d
wra <lovemode1993@naver.com>
2020-01-21 14:08:42 +0900

    S02P12A305-4 Setting

--- Backend/SSAFYSNS/src/main/java/com/ssafysns/model/dto/User.java
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/model/dto/User.java
@@ -151,7 +151,7 @@ public class User {
 	private String auth;
 
 	@Column()
-	boolean deleted;
+	private boolean deleted;
 
 //	@Builder
 //	public User(String id, String no, String name, String password, String phone, Date birth, String nickname,
