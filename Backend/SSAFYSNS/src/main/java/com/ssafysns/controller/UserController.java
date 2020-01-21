59baf0ceea94030e11f490c6cee7b5ca48e1f9ba
sujinhope <hope772948@gmail.com>
2020-01-21 12:18:10 +0900

    S02P12A305-4 [Modify] Controller Package

--- Backend/SSAFYSNS/src/main/java/com/ssafysns/controller/UserController.java
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/controller/UserController.java
@@ -1,20 +1,13 @@
 package com.ssafysns.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //import com.ssafysns.model.service.UserService;
@@ -28,7 +21,6 @@ public class UserController {
 	
 	@Autowired
 //	private UserService service;
-//	private FAQRepository repo;
 	
 	@ExceptionHandler
 	public ResponseEntity<Map<String, Object>> handler(Exception e){
--- Backend/SSAFYSNS/src/main/java/com/ssafysns/model/service/UserServiceImpl.java
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/model/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.ssafysns.model.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ssafysns.dto.User;
+import com.ssafysns.model.dto.User;
 import com.ssafysns.repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService{
