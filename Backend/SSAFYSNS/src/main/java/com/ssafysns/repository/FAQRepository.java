1aaa644b1f6afefee09046bf6431f928087b2f35
sujinhope <hope772948@gmail.com>
2020-01-22 15:22:57 +0900

    S02P12A305-98 FAQ

--- Backend/SSAFYSNS/src/main/java/com/ssafysns/repository/FAQRepository.java
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/repository/FAQRepository.java
@@ -1,6 +1,11 @@
 package com.ssafysns.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ssafysns.model.dto.FAQ;
@@ -8,6 +13,10 @@ import com.ssafysns.model.dto.FAQ;
 @Repository
 public interface FAQRepository extends JpaRepository<FAQ, Integer> {
 
-	FAQ findById(String id);
+	List<FAQ> findById(String id);
+	
+	@Modifying
+	@Query("update FAQ f set f.deleted = true where f.no=:no")
+	void updateDeleted(@Param("no") int no);
 
 }
