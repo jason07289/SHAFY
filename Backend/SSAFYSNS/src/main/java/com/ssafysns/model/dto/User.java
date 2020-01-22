e2a2943dd6610daf9f20133c36371506206c7951
jason07999 <jason07999@naver.com>
2020-01-22 14:40:18 +0900

    -conflict 해결

--- Backend/SSAFYSNS/src/main/java/com/ssafysns/model/dto/User.java
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/model/dto/User.java
@@ -107,11 +107,8 @@ public class User {
 	@Column(length = 20, nullable = false)
 	private String name;
 
-<<<<<<< HEAD
+
 	@Column(length = 200,nullable = false)
-=======
-	@Column(nullable = false)
->>>>>>> 720e97187a29c7a7f62c21ec27b76c75714e435e
 	private String password;
 
 //	@Getter
@@ -159,19 +156,6 @@ public class User {
 	
 	@Column(columnDefinition = "TINYINT", length=1)
 	private int deleted;
-
-	
-
-
-<<<<<<< HEAD
-=======
-	@Column(columnDefinition = "TINYINT", length=1)
-	private int deleted;
-
-
-	
-
->>>>>>> 720e97187a29c7a7f62c21ec27b76c75714e435e
 
 	// 외래키 설정
 //	@OneToMany(mappedBy = "user")
