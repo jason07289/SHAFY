8885c3b1fdea77a71bb7e5f15c34532f73f67405
SOOJIN <soojin4964@gmail.com>
2020-02-07 10:35:53 +0900

    10:35 login test

--- Frontend/frontend/src/apis/SNSApi.js
+++ Frontend/frontend/src/apis/SNSApi.js
@@ -5,31 +5,31 @@ const hosturl = 'http://i02a305.p.ssafy.io:8080'
 const appname = '/userSNS'
 
 const requestgithubLogin = (params, callback, errorCallback) => {
-  axios.get(hosturl+appname, params)
+  axios.get(hosturl+appname+`githubLogin/${params.code}`)
   .then(callback)
   .catch(errorCallback)
 }
 
 const requestgoogleLogin = (params, callback, errorCallback) => {
-  axios.get(hosturl+appname, params)
+  axios.get(hosturl+appname+`googleLogin/${params.code}`)
   .then(callback)
   .catch(errorCallback)
 }
 
 const requestkakaoLogin = (params, callback, errorCallback) => {
-  axios.get(hosturl+appname, params)
+  axios.get(hosturl+appname+`kakaoLogin/${params.code}`)
   .then(callback)
   .catch(errorCallback)
 }
 
 const requestnaverLogin = (params, callback, errorCallback) => {
-  axios.get(hosturl+appname, params)
+  axios.get(hosturl+appname+`naverLogin/${params.code}`)
   .then(callback)
   .catch(errorCallback)
 }
 
 const SNSApi = {
-  requestgithubLogin:(data,callback,errorCallback)=>requestgithubLogin(data,callback,errorCallback),
+  requestgithubLogin:(params,callback,errorCallback)=>requestgithubLogin(params,callback,errorCallback),
   requestgoogleLogin:(params, callback, errorCallback) =>requestgoogleLogin(params, callback, errorCallback),
   requestkakaoLogin:(params, callback, errorCallback) =>requestkakaoLogin(params, callback, errorCallback),
   requestnaverLogin:(params, callback, errorCallback) =>requestnaverLogin(params, callback, errorCallback)
