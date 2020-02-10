ebbccf83221f3f3266c79d437c2284d16458ea53
milkbean <nim950313@gmail.com>
2020-02-10 17:12:36 +0900

    Post.vue 데이터 뿌리기

--- Frontend/frontend/src/apis/PostApi.js
+++ Frontend/frontend/src/apis/PostApi.js
@@ -3,7 +3,7 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(
 
 
 // const hosturl = 'http://i02a305.p.ssafy.io'
-const hosturl = 'http://70.12.246.122:8080'
+const hosturl = 'http://70.12.247.81:8080'
 const appname = '/post'
 
 /* eslint-disable no-unused-vars */
