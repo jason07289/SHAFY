5529b5659de52f15d4cdf2834cf1bbda6d78eb37
SOOJIN <soojin4964@gmail.com>
2020-02-13 09:45:12 +0900

    S02P13A305 S02P13A305-62 Actions (Type . to access issue actions) Img upload in Join page

--- Frontend/frontend/src/main.js
+++ Frontend/frontend/src/main.js
@@ -5,12 +5,17 @@ import routes from './routes'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import Vuetify from 'vuetify/lib'
-
+import firebase from 'firebase'
+import firebaseconfig from './plugins/firebaseconfig'
+import ImageUploader from 'vue-image-upload-resize'
 
 Vue.config.productionTip = false
 
 Vue.use(Router)
 Vue.use(Vuetify)
+Vue.use(ImageUploader)
+firebase.initializeApp(firebaseconfig)
+
 
 const router = new Router({
   routes,
