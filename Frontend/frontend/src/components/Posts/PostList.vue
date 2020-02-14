2269689e82db4b9594a4407f5dda013aabea444d
milkbean <nim950313@gmail.com>
2020-02-14 18:23:40 +0900

    공백뜨는거해결

--- Frontend/frontend/src/components/Posts/PostList.vue
+++ Frontend/frontend/src/components/Posts/PostList.vue
@@ -1,29 +1,24 @@
 <template>
   <div style="background-color:var(--background-w);">
-    <span class = "overline" style="padding-left:40%;">{{tabName}}탭의 PostList </span>
-    <!-- <div style="width:100%" v-for="post in posts" :key="post.id">
-      {{post}}
-    </div> -->
-    <Post v-for="post in posts"
-    :key="post.pno" :post="post"/>
+   <span class="title"> {{tabName}} 탭이 생성됨 </span>
   </div>
 </template>
 
 <script>
-import Post from './Post'
 import { mapState } from 'vuex'
 
 export default {
   name: 'PostList',
-  components:{
-    Post,
-  },
   computed:
     mapState({
     posts: state => state.post.posts
   }),
   created(){
     this.$store.dispatch('post/getAllPosts')
+  },
+  mounted(){
+    console.log(this.tabName+"탭이 생성됨")
+    
   },
   props: ['tabName']
   
--- Frontend/frontend/src/views/home/Home.vue
+++ Frontend/frontend/src/views/home/Home.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-app >
-    <template slot:extension >
+  <v-app  >
         <v-tabs
           v-model="tab"
           align-with-title
@@ -14,22 +13,18 @@
           <v-tab v-for="item in Tabs" :key="item">
             <v-icon size="17">mdi-pound</v-icon>{{ item }}
           </v-tab>
-        </v-tabs>
-      </template>
+        
 
       <v-tabs-items v-model="tab" >
       <v-tab-item
         v-for="item in Tabs"
         :key="item"
       >
-    <PostList
-    v-infinite-scroll="loadMore" 
-    infinite-scroll-disabled="busy" 
-    infinite-scroll-distance="10" 
-    :tab-name="item"></PostList>
+    <PostList :tab-name="item"></PostList>
         
       </v-tab-item>
     </v-tabs-items>
+    </v-tabs>
 
 
   </v-app>
@@ -49,6 +44,8 @@ export default {
         //   '다른태그', '삼성전자', '싸피2기', '싸피셜','알고리즘스터디','취업준비','도커공부하는애들','카페','식단',
         // ],
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        tabName:'',
+        compoName:'',
       }
   },
   components:{
@@ -64,6 +61,13 @@ export default {
      console.log(this.page)
      console.log('읽어져라')
      this.getAllPosts({page: this.page})
+    },
+    //수정 코드----
+    clickTab(name){
+      console.log("생성됨>>"+name)
+      this.compoName = ''
+      this.compoName = 'PostList'
+      this.tabName = name;
     }
   },
   created(){
@@ -94,11 +98,5 @@ div.v-tabs-slider-wrapper{
 div.v-tabs-slider-wrapper > div{
   border-radius: 13px 13px 0px 0px;
   
-}
-.v-window{
- /* 본문 부분.. border 시도했으나 실패 */
-}
-.v-tab:not(.v-tab--active){
-  color:text-#ffffff;
 }
 </style>
