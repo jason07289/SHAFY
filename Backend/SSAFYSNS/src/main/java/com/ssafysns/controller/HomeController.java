ee46c9c636d14ee03dc1e703a4276e0320614d88
jason07289 <jason07999@naver.com>
2020-03-02 19:34:33 +0900

    제발

---
+++ Backend/SSAFYSNS/src/main/java/com/ssafysns/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.ssafysns.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//http://localhost/home/settags
+@Controller
+public class HomeController implements ErrorController{
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/error")
+	public String redirect() {
+		return "index";
+	}
+	
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+	
+  
+}
